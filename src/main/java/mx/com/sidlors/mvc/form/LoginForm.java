9658a76908e991fd84798a1517e4e0061ff93f3b
andresaramos <andres.alberto.ramos@gmail.com>
2023-10-10 08:32:31 -0300

    First commit

---
+++ src/main/java/mx/com/sidlors/mvc/form/LoginForm.java
@@ -0,0 +1,18 @@
+package mx.com.sidlors.mvc.form;
+public class LoginForm {
+	private String userName;
+	private String password;
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPassword() {
+		return password;
+	}
+}
